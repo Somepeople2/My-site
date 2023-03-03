18745338c857a0ee32221cdaeeba6fd6c812d33c
K.Alex <creepking@mail.ru>
2023-03-03 19:35:05 +0300

    more stuffsagaefv

---
+++ js/events/events.js
@@ -0,0 +1,11 @@
+function age() {
+    let age = Number(prompt('How old are you?'))
+    alert(`in 10 years you are going to be ${age} years old.`)
+}
+
+function sheep() {
+    let count = Number(prompt('How many sheep are there'))
+    for (let i = 1; i<=count; i++) {
+        alert(`${i} sheep`)
+    }
+}
--- js/rangeFilter.js/rangeFilter.js
+++ js/rangeFilter.js/rangeFilter.js
@@ -4,4 +4,4 @@ function rangeFilter(arr,a,b) {
 
 let list = [5, 3, 8, 1];
 alert(rangeFilter(list,1,4));
-alert(list);
+alert(list);a
